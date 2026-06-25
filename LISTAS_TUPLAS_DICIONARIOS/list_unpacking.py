c036884ed6c31fd76b7b9297d1712816cf6f3ec4
wagaum <138220357+hostcon@users.noreply.github.com>
2026-06-25 14:19:03 -0300

    Macete unpacking

--- LISTAS_TUPLAS_DICIONARIOS/list_unpacking.py
+++ LISTAS_TUPLAS_DICIONARIOS/list_unpacking.py
@@ -16,3 +16,12 @@ nome, sobrenome, *_, idade = infos
 # Exibindo o resultado
 print(f"{nome} {sobrenome} tem {idade} anos")
 
+'''
+Como funciona o truque?
+nome, sobrenome: Capturam, respectivamente, os dois primeiros itens da lista ("João" e "Paulo") [00:15].
+
+*_: O asterisco (*) diz ao Python para agrupar o restante dos elementos em uma lista, e o underline (_) é uma convenção na programação para indicar uma variável que será descartada (ignorada) [00:18].
+
+idade: Como está posicionada após o *_, ela captura o último elemento da lista ("25") [00:24].
+'''
+
